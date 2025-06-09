7d2651ee8158b0a8ac9532e00de55a060bbbed27
Jimi Joe <tano777864@gmail.com>
2025-06-09 12:34:15 -0400

    base de proyecto

---
+++ springboot-app/src/main/java/com/information/spring/app1/springboot_app/SpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package com.information.spring.app1.springboot_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAppApplication.class, args);
+	}
+
+}
---
+++ springboot-app/src/test/java/com/information/spring/app1/springboot_app/SpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.information.spring.app1.springboot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
