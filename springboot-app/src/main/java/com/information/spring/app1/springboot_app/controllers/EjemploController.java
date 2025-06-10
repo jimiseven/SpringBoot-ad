f6ee9430c48268d9bbf6d385940545d53009332f
Jimi Joe <tano777864@gmail.com>
2025-06-10 10:44:42 -0400

    injeccion de dependencias

--- springboot-app/src/main/java/com/information/spring/app1/springboot_app/controllers/EjemploController.java
+++ springboot-app/src/main/java/com/information/spring/app1/springboot_app/controllers/EjemploController.java
@@ -1,6 +1,7 @@
 package com.information.spring.app1.springboot_app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
 
@@ -8,11 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class EjemploController {
     @GetMapping("/detalles_claseInfo")
-    // public String getMethodName(@RequestParam String param) {
-    //     return new String();
-    // }
     
-    public String info(){
+    public String info(Model model){
+        model.addAttribute("titulo","server on line");
+        model.addAttribute("servidor","informatic config");
+        model.addAttribute("ip","192.168.101.0");
         return "detalles_claseInfo";
     }
 }
