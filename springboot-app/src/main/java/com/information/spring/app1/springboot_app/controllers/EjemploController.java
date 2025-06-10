37228a91725e4deb27830c0067f6196596ddb7f8
Jimi Joe <tano777864@gmail.com>
2025-06-10 12:14:02 -0400

    uso de objeto model con datos

--- springboot-app/src/main/java/com/information/spring/app1/springboot_app/controllers/EjemploController.java
+++ springboot-app/src/main/java/com/information/spring/app1/springboot_app/controllers/EjemploController.java
@@ -5,15 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
 
+import com.information.spring.app1.springboot_app.models.Empleados;
+
 
 @Controller
 public class EjemploController {
     @GetMapping("/detalles_claseInfo")
     
     public String info(Model model){
-        model.addAttribute("titulo","server on line");
-        model.addAttribute("servidor","informatic config");
-        model.addAttribute("ip","192.168.101.0");
+        // HARD CODEANDO DATOS PARA DEMOSTRAR EL USO DE LA VISTA        
+        // model.addAttribute("titulo","server on line");
+        // model.addAttribute("servidor","informatic config");
+        // model.addAttribute("ip","192.168.101.0");
+
+        Empleados empleado1 = new Empleados("jimi", "torrico", "av galindo", "junior", 11, 76992490, 11);
+        model.addAttribute("Titulo", "Informacion del Empleado");
+        model.addAttribute("Empleado", empleado1);
         return "detalles_claseInfo";
     }
 }
--- springboot-app/src/main/java/com/information/spring/app1/springboot_app/controllers/EjemploRestController.java
+++ springboot-app/src/main/java/com/information/spring/app1/springboot_app/controllers/EjemploRestController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.information.spring.app1.springboot_app.models.Empleados;
+
 
 
 
@@ -17,12 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class EjemploRestController {
 @GetMapping(path = "/detalles_claseInfo2")
 
-    
+
+
     public Map<String, Object> detalles_claseInfo2(){
+
+        Empleados empleado1 = new Empleados("jimi", "torrico", "av galindo", "junior", 11, 76992490, 11);
+
         Map<String, Object> respuesta = new HashMap<>();
-        respuesta.put("titulo","server on line");
-        respuesta.put("servidor","informatic config");
-        respuesta.put("ip","192.168.101.0");
+        respuesta.put("Empleado",empleado1);
         return respuesta;
     }
 }
