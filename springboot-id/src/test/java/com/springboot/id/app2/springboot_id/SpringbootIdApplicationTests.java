563ccff638e2aa87e464d1fd5357cf4ef14076ac
Jimi Joe <tano777864@gmail.com>
2025-06-13 12:43:55 -0400

    creacion de proyecto para inyeccion de dependencias

---
+++ springboot-id/src/test/java/com/springboot/id/app2/springboot_id/SpringbootIdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.id.app2.springboot_id;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
