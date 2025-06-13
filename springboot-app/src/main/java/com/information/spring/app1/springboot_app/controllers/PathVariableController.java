fb59ebe6053cf9266d87f122c3c638c477d5126e
Jimi Joe <tano777864@gmail.com>
2025-06-13 12:18:43 -0400

    uso de value, en applications properties

--- springboot-app/src/main/java/com/information/spring/app1/springboot_app/controllers/PathVariableController.java
+++ springboot-app/src/main/java/com/information/spring/app1/springboot_app/controllers/PathVariableController.java
@@ -5,10 +5,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.information.spring.app1.springboot_app.models.Empleados;
 
+import java.lang.foreign.ValueLayout;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -16,6 +23,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/api/variable")
 public class PathVariableController {
+
+    @Value("${config.codigo}")
+    private int [] codigo;
+    @Value("${config.usuario}")
+    private String [] usuario;
+    @Value("${config.mensaje}")
+    private String [] mensaje;
+    @Value("${config.valores}")
+    private String [] valores;
+
+    
+    
+
     @GetMapping("/pagina1/{mensaje}")
     public ParametroDTO pagina1(@PathVariable String mensaje) {
 
@@ -30,6 +50,15 @@ public class PathVariableController {
         return empleado1;
     }
     
+    @GetMapping("/valores")
+    public Map<String, Object> values() {
+        Map<String, Object> json = new HashMap<>();
+        json.put("codigo", codigo);
+        json.put("usuario", usuario);
+        json.put("mensaje", mensaje);
+        json.put("valores", valores);
+        return json;
+    }
     
     
 }
