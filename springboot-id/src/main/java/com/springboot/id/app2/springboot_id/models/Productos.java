d74a4ea814ac16e84d8685784c5ccd3546793e42
Jimi Joe <tano777864@gmail.com>
2025-06-14 08:45:06 -0400

    manejo de data con repos

---
+++ springboot-id/src/main/java/com/springboot/id/app2/springboot_id/models/Productos.java
@@ -0,0 +1,33 @@
+package com.springboot.id.app2.springboot_id.models;
+
+public class Productos {
+
+    private Long idProducto;
+    private String nombre;
+    private double precio;
+
+    public Productos(Long idProducto, String nombre, String precio) {
+        this.idProducto = idProducto;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public Long getIdProducto() {
+        return idProducto;
+    }
+    public void setIdProducto(Long idProducto) {
+        this.idProducto = idProducto;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+}
---
+++ springboot-id/src/main/java/com/springboot/id/app2/springboot_id/repositorios/Repo_Productos.java
@@ -0,0 +1,24 @@
+package com.springboot.id.app2.springboot_id.repositorios;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.springboot.id.app2.springboot_id.models.Productos;
+
+public class Repo_Productos {
+
+    List<Productos> datos;
+
+    public Repo_Productos(List<Productos> datos) {
+        this.datos = Arrays.asList(null,
+                new Productos(1L, "Producto 1", "10.00"),
+                new Productos(2L, "Producto 2", "20.00"),
+                new Productos(3L, "Producto 3", "30.00"),
+                new Productos(4L, "Producto 4", "40.00"),
+                new Productos(5L, "Producto 5", "50.00"));
+    }
+
+    public List<Productos> findAll() {
+        return datos;
+    }
+}
